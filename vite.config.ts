981f448bed0402ac68fb2f883327625f10d2eba9
Manish Mawatwal <connection2manish@gmail.com>
2026-07-31 13:30:18 -0400

    Fix base path for kidsmagic repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/math-practice-app/',
+  base: '/kidsmagic/',
   plugins: [react()],
   resolve: {
     alias: {
